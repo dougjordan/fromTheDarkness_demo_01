74e08c02fe9a118a6583109e376cc15c0f596351
dougjordan <dou9.jordan@gmail.com>
2014-11-11 23:43:03 +0000

    Phase 1 Maze generator
    http://catlikecoding.com/unity/tutorials/maze/

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Up and running");
+        BeginGame();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Restart");
+            RestartGame();
+        }
+    }
+
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
---
+++ Assets/Scripts/Maze.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze : MonoBehaviour {
+
+    // Based on http://catlikecoding.com/unity/tutorials/maze/
+
+    public IntVector2 size;
+
+    //public int sizeX, sizeZ;
+
+    public MazeCell cellPrefab;
+
+    private MazeCell[,] cells;
+
+    public float generationStepDelay;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public MazeCell GetCell(IntVector2 coordinates) {
+        return cells[coordinates.x, coordinates.z];
+    }
+
+    public IEnumerator Generate()
+    {
+        WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
+        cells = new MazeCell[size.x, size.z];
+
+        IntVector2 coordinates = RandomCoordinates;
+        while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null)
+        {
+            yield return delay;
+            CreateCell(coordinates);
+            coordinates += MazeDirections.RandomValue.ToIntVector2();
+        }
+
+        //for (int x = 0; x < size.x; x++)
+        //{
+        //    for (int z = 0; z < size.z; z++)
+        //    {
+        //        yield return delay;
+        //        CreateCell(new IntVector2(x, z));
+        //    }
+        //}
+    }
+
+    private void CreateCell(IntVector2 coordinates)
+    {
+        MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
+        cells[coordinates.x, coordinates.z] = newCell;
+        newCell.coordinates = coordinates;
+        newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
+        newCell.transform.parent = transform;
+        newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+    }
+
+    public IntVector2 RandomCoordinates {
+        get { 
+            return new IntVector2(Random.Range(0, size.x), Random.Range(0, size.z));
+        }
+    }
+
+    public bool ContainsCoordinates(IntVector2 coordinates) {
+        return coordinates.x >= 0 && coordinates.x < size.x && coordinates.z >= 0 && coordinates.z < size.z;
+    }
+
+}
